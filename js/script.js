92a1017ba86dd2626c3d5cd136b1174c4d059fba
Theodoric-Graham <theodoricgraham@hotmail.com>
2022-02-19 16:12:36 -0600

    implementing smooth scrolling

--- js/script.js
+++ js/script.js
@@ -17,6 +17,46 @@ btnNavEl.addEventListener("click", function () {
 });
 
 ///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+//click event on anchor elements using event delegation
+window.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (
+    !e.target.getAttribute("href") &&
+    !e.target.parentNode.getAttribute("href")
+  )
+    return;
+  else {
+    //getting href of image anchor element
+    let href = e.target.getAttribute("href");
+    if (e.target.tagName !== "A") {
+      href = e.target.parentNode.getAttribute("href");
+
+      //Scroll back to top
+      if (href === "#")
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+    }
+
+    //Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+
+    //Close mobile navigation
+    if (e.target.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  }
+});
+
+///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
